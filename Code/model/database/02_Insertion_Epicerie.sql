79a5b561187eb9aaa3f9a7d6adf61c60a17cd74f
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-07 10:38:24 +0200

    fixe

--- Code/model/database/02_Insertion_Epicerie.sql
+++ Code/model/database/02_Insertion_Epicerie.sql
@@ -1,13 +1,14 @@
 /*
 Gamme
-*/
+
 INSERT INTO Gamme
 VALUES
 SELECT * FROM Gamme;
 
 /*
 Article
-*/
+
 INSERT INTO Article
 VALUES
 SELECT * FROM Article;
+*/
--- Code/model/database/02_Insertion_Patisserie.sql
+++ Code/model/database/02_Insertion_Patisserie.sql
@@ -1,13 +1,14 @@
 /*
 Categorie
-*/
+
 INSERT INTO Catégorie
 VALUES
 SELECT * FROM Categorie;
 
 /*
 Patisserie
-*/
+
 INSERT INTO Patisserie
 VALUES
 SELECT * FROM Patisserie;
+*/
