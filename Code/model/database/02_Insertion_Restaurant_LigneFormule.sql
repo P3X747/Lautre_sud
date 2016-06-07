3de2b584f9051c211f0dd90ff5ada192e23b6fe0
HighX97 <loic.ortole@gmail.com>
2016-06-07 11:39:07 +0200

    Insetion restaurant 13%

--- Code/model/database/02_Insertion_Restaurant_LigneFormule.sql
+++ Code/model/database/02_Insertion_Restaurant_LigneFormule.sql
@@ -1,50 +1,6 @@
 /*
-Formule
-
-INSERT INTO Formule
-VALUES
-SELECT * FROM Formule;
-
-*/
-
-/*
-Type
-libelle varchar(255),
-description varchar(255),
-*/
-INSERT INTO Type
-(libelle)
-VALUES
-("Entrée")
-;
-INSERT INTO Type
-(libelle)
-VALUES
-("Plat")
-;
-INSERT INTO Type
-(libelle)
-VALUES
-("Dessert")
-;
-INSERT INTO Type
-(libelle)
-VALUES
-("Boisson")
-;
-SELECT * FROM Type;
-
-/*
 Ligne_formule
 
 INSERT INTO Ligne_formule
 VALUES
 SELECT * FROM Ligne_formule;
-
-/*
-Element_carte
-
-INSERT INTO Element_carte
-VALUES
-SELECT * FROM Element_carte;
-*/
