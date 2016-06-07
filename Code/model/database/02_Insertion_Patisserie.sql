f1e8c39cf4f24c0dba5f6016975377697e84e47f
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-07 11:31:09 +0200

    Insertion patisserie 75%

--- Code/model/database/02_Insertion_Patisserie.sql
+++ Code/model/database/02_Insertion_Patisserie.sql
@@ -1,14 +1,60 @@
 /*
 Categorie
 
-INSERT INTO Catégorie
-VALUES
+INSERT INTO Catégorie (libelle, description, prix)
+VALUES (,1.80);
 SELECT * FROM Categorie;
+*/
 
 /*
 Patisserie
-
-INSERT INTO Patisserie
-VALUES
-SELECT * FROM Patisserie;
 */
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Baclawa amandes, pistaches ou noix", "Pâtes feuilletées croustillantes farcies de fruits secs concassés et arrosées de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Chebakia sésame et miel:","Gâteau enrobé de sésame, arrosé de miel et légèrement parfumé à l'anis",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Cigare noix de cajous ou amandes", "Pâte feuilletées farcies de noix de cajous et arrosées de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Corne de gazelle libanaise:","Biscuit sablé farci d'amandes et aux éclats de noix, enrobé de sucre glace.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Corne de gazelle marocaine","Biscuit fondant farci d'amandes et parfumé à la fleur d'oranger.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Corne de gazelle tunisienne","Biscuit sablé farci d'amandes concassés et enrobés de sucre glace.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Galbellouz semoule et miel","Semoule et miel au coeur fondant à l'amande et parfumé à la fleur d'oranger.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Kadaïf noix ou amandes:","Cheveux d'ange farcis de fruits secs concassés et de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Losanges amandes, pistaches ou noix:","Pâtes feuilletées fondantes farcies de fruits secs finement concassés et arrosés de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Makroudhs dattes:","Semoule de blé dur, farci de pâte de datte et enrobé de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Mamoul datte, pistaches ou noix","Biscuit sablé libanais farci à la datte, pistache ou noix.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Montecao cannelle:","Biscuit sablé parfumé à la cannelle.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Nid de pistache:", "Pâte d'amande et de noix enrobée de pistaches concassées et de sucre glace.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Nid d'oiseau:","Cheveux d'ange garnis de pistaches entières caramélisées.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Wardi pistache:","Pâtes feuilletés avec des pistaches entières arrosées de miel.",1.80);
+
+INSERT INTO Patisserie(libelle, description, prix)
+VALUES ("Zlabia","Beignet de miel souvent coloré en rouge.",1.80);
+
+SELECT * FROM Patisserie;
