0feab057fa19fa115b135e08cd85497a16622a2f
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-07 12:02:18 +0200

    Insertion restaurant élément carte 60%

--- Code/model/database/02_Insertion_Restaurant_ElementCarte.sql
+++ Code/model/database/02_Insertion_Restaurant_ElementCarte.sql
@@ -5,3 +5,26 @@ INSERT INTO Element_carte
 VALUES
 SELECT * FROM Element_carte;
 */
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Couscous","Végétarien",8);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Couscous","Agneau, veau, poulet, merguez",10);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Couscous","Royal (Trois viandes au choix)",11);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Tajine","Poulet, carottes et olives vertes",11);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Tajine","Agneau et prunneaux",9);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Lasagne libanaise (Kafta bel sayniyé):","Viande hachée de boeuf, aubergines, pomme de terre et tomate disposés en couches horizontales, cuits au four et accompagnés de riz à la vermicelle ou de la semoule.",9);
+
+INSERT INTO Element_carte (libelle, description, prix)
+VALUES ("Aubergines farcies (Cheikh el mehché):","Aubergines grillées au four et farcies de viandes finement hachées de boeuf et sauce tomate le tout accompagné de riz à la vermicelle.",9);
+
+SELECT * FROM Element_carte;
