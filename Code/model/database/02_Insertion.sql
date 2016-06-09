4033909a83047e1358a57aaa35836011dc3abd54
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-09 10:16:20 +0200

    Insertion restaurant élements carte 90%

--- Code/model/database/02_Insertion.sql
+++ Code/model/database/02_Insertion.sql
@@ -7,16 +7,16 @@ Tables concernant l'Entreprise
 Entreprise
 */
 INSERT INTO Entreprise (libelle)
-VALUES ('L\'autre Sud');
+VALUES ("L'autre Sud");
 SELECT * FROM Entreprise;
 
 /*
 Mail
 */
 INSERT INTO Mail (valeur, entreprise)
-VALUES ('contact@lautresud.com','L\'autre Sud');
+VALUES ("contact@lautresud.com","L'autre Sud");
 INSERT INTO Mail (valeur, entreprise)
-VALUES ('lautresud@gmail.com','L\'autre Sud');
+VALUES ("lautresud@gmail.com","L'autre Sud");
 
 SELECT * FROM Mail;
 
@@ -25,7 +25,7 @@ Adresse
 */
 
 INSERT INTO Adresse (valeur, entreprise)
-VALUES ('L\'autre Sud, 26 rue des Etuves 34000 Montpellier','L\'autre Sud');
+VALUES ("L'autre Sud, 26 rue des Etuves 34000 Montpellier","L'autre Sud");
 SELECT * FROM Adresse;
 
 
@@ -33,9 +33,9 @@ SELECT * FROM Adresse;
 Telephone
 */
 INSERT INTO Telephone (numero, entreprise)
-VALUES ('06 99 77 95 89','L\'autre Sud');
+VALUES ("06 99 77 95 89","L'autre Sud");
 INSERT INTO Telephone (numero, entreprise)
-VALUES ('04 67 56 23 94','L\'autre Sud');
+VALUES ("04 67 56 23 94","L'autre Sud");
 SELECT * FROM Telephone;
 
 source 02_Insertion_Epicerie.sql;
