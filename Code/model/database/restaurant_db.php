4edce36476277feb894462c13750b5a0a00bbe17
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-15 10:46:31 +0200

    restaurant_db getFormule getType

--- Code/model/database/restaurant_db.php
+++ Code/model/database/restaurant_db.php
@@ -18,11 +18,43 @@ class Restaurant_db extends  Abstract_db
       return $resultat;
     }
 
+
+
+    public function getType()
+    {
+      // Enregistrement de la requete
+      $stmt = $this->conn->prepare("SELECT *
+        FROM Type");
+        //Exécution de la requete
+        $stmt->execute();
+        //Récupération du résultat dans la variable resultat
+        $resultat = $stmt->get_result();
+        //Fermeture de la connection avec la base de données MySql
+        $stmt->close();
+        //Retour de la variable resultat
+        return $resultat;
+      }
+
+      public function getFormule()
+      {
+        // Enregistrement de la requete
+        $stmt = $this->conn->prepare("SELECT *
+          FROM Formule");
+          //Exécution de la requete
+          $stmt->execute();
+          //Récupération du résultat dans la variable resultat
+          $resultat = $stmt->get_result();
+          //Fermeture de la connection avec la base de données MySql
+          $stmt->close();
+          //Retour de la variable resultat
+          return $resultat;
+        }
+
     public function affichage_liste_Cours()
     {
-      //Appel de la fonction getAllElementCarte()
+      //Appel de la fonction getAllElementCarte ()
       //Enregistrement du résultat dans la variable $rsltCours
-      $rsltCours = $this->getAllElementCarte();
+      $rsltCours = $this->getAllElementCarte ();
       //Si il y a au moins une ligne
       if ($rsltCours->num_rows > 0) {
         //Variable $i enregistre le numero de la ligne à afficher
