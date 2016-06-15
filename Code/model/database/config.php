570d712fbd08320c32790d7bda2a2a4893678077
HighX97 <loic.ortole@gmail.com>
2016-06-15 10:45:42 +0200

    Db_Connect and Abstact_db refactoring

--- Code/model/database/config.php
+++ Code/model/database/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'gazlot');
+define('DB_PASSWORD', 'password');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'db_lautre_sud');
 ?>
--- Code/model/database/db_connect.php
+++ Code/model/database/db_connect.php
@@ -1,9 +1,6 @@
 <?php
 class Db_Connect
 {
-    //Variable permettant d'enregiter la connection
-    private $conn;
-
     //Fonction gérant la création d'une connection
     function connect()
     {
@@ -11,13 +8,13 @@ class Db_Connect
         include_once dirname(__FILE__) . '/Config.php';
 
         // Connection à la base de données mysql
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
         // Traitement du cas d'erreur de la fonction de connecion
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
-        return $this->conn;
+        return $conn;
     }
 }
 ?>
