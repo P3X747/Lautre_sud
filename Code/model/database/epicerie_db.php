f712a04e14a7165aa38374ec942dd5e835c44e32
HighX97 <loic.ortole@gmail.com>
2016-06-16 11:24:14 +0200

    Epicerie db-view-template

---
+++ Code/model/database/epicerie_db.php
@@ -0,0 +1,39 @@
+<?php
+require_once('abstract_db.php');
+class Epicerie_db extends  Abstract_db
+{
+
+  public function getArticle()
+  {
+    // Enregistrement de la requete
+    $stmt = $this->conn->prepare("SELECT *
+      FROM article
+      ORDER BY gammme");
+      //Exécution de la requete
+      $stmt->execute();
+      //Récupération du résultat dans la variable resultat
+      $resultat = $stmt->get_result();
+      //Fermeture de la connection avec la base de données MySql
+      $stmt->close();
+      //Retour de la variable resultat
+      return $resultat;
+    }
+
+    public function getGamme()
+    {
+      // Enregistrement de la requete
+      $stmt = $this->conn->prepare("SELECT *
+        FROM Gamme");
+        //Exécution de la requete
+        $stmt->execute();
+        //Récupération du résultat dans la variable resultat
+        $resultat = $stmt->get_result();
+        //Fermeture de la connection avec la base de données MySql
+        $stmt->close();
+        //Retour de la variable resultat
+        return $resultat;
+      }
+
+
+  }
+  ?>
---
+++ Code/view/epicerie_template.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+
+    <!--[if lt IE 9]>
+    <html class="lt-ie9">
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <script src='js/device.min.js'></script>
+</head>
+
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+    <header>
+
+        <div id="stuck_container" class="stuck_container">
+            <div class="container">
+
+                <div class="brand">
+                    <h1 class="brand_name">
+                        <a href="./">Cafe</a>
+                    </h1>
+                </div>
+                <?php
+                  include "nav_bar_template.php"
+                 ?>
+            </div>
+        </div>
+
+    </header>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main>
+        <section class="well well__offset-3">
+            <div class="container">
+                <h2><em>L'autre Sud</em>Menu</h2>
+                <?php
+                /*
+          				require_once("../../model/database/restaurant_db.php");
+          				$coursModel = new Restaurant_db();
+          				$coursModel->affichage_liste_Cours();
+                  */
+                  require_once("/epicerie_view.php");
+          				$restaurant_view = new Epicerie_view();
+                  echo "<h2><em>Gammes</em></h2>";
+                  $restaurant_view->display_list_Gamme();
+                  echo "<h2><em>Article</em></h2>";
+          				$restaurant_view->display_list_Article();
+          				?>
+            </div>
+        </section>
+    </main>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <footer>
+        <div class="container">
+            <ul class="socials">
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-tumblr"></a></li>
+                <li><a href="#" class="fa fa-google-plus"></a></li>
+            </ul>
+            <div class="copyright">© <span id="copyright-year"></span> |
+                <a href="#">Privacy Policy</a>
+            </div>
+        </div>
+        <div class="tm"><a href="#"><img src="images/TM_logo.png" alt=""></a></div>
+    </footer>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
