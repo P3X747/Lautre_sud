ddf4442589e38ea871d14b66db1173855270c0dd
HighX97 <loic.ortole@gmail.com>
2016-06-16 09:43:06 +0200

    view directory refactoring

---
+++ Code/view/index-3.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+
+    <!--[if lt IE 9]>
+    <html class="lt-ie9">
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <script src='js/device.min.js'></script>
+</head>
+
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+    <header>
+
+        <div id="stuck_container" class="stuck_container">
+            <div class="container">
+
+                <div class="brand">
+                    <h1 class="brand_name">
+                        <a href="./">Cafe</a>
+                    </h1>
+                </div>
+
+                <nav class="nav">
+                    <ul class="sf-menu">
+                        <li>
+                            <a href="./">Home</a>
+                        </li>
+                        <li>
+                            <a href="index-1.html">About</a>
+                            <ul>
+                                <li>
+                                    <a href="#">Quisque nulla</a>
+                                </li>
+                                <li>
+                                    <a href="#">Vestibulum libero</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Lorem</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Dolor</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Sit amet</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Vivamus eget nibh</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="index-2.html">What We Do</a>
+                        </li>
+                        <li class="active">
+                            <a href="index-3.html">Menu</a>
+                        </li>
+                        <li>
+                            <a href="index-4.html">Contacts</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </header>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main>
+        <section class="well well__offset-3">
+            <div class="container">
+                <h2><em>L'autre Sud</em>Menu</h2>
+                <?php
+                /*
+          				require_once("../../model/database/restaurant_db.php");
+          				$coursModel = new Restaurant_db();
+          				$coursModel->affichage_liste_Cours();
+                  */
+                  require_once("/restaurant_view.php");
+          				$restaurant_view = new Restaurant_view();
+                  echo "<h2><em>Types</em></h2>";
+                  $restaurant_view->display_list_Type();
+                  echo "<h2><em>Formules</em></h2>";
+                  $restaurant_view->display_list_Formule();
+                  echo "<h2><em>Carte</em></h2>";
+          				$restaurant_view->display_list_ElementCarte();
+          				?>
+            </div>
+        </section>
+    </main>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <footer>
+        <div class="container">
+            <ul class="socials">
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-tumblr"></a></li>
+                <li><a href="#" class="fa fa-google-plus"></a></li>
+            </ul>
+            <div class="copyright">© <span id="copyright-year"></span> |
+                <a href="#">Privacy Policy</a>
+            </div>
+        </div>
+        <div class="tm"><a href="#"><img src="images/TM_logo.png" alt=""></a></div>
+    </footer>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
