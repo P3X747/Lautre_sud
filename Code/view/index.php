cd95ec70c0b23b363e40d86611bb098c65d8059e
Pierre-Henri Traboulsi <Pierre-Henri Traboulsi>
2016-06-16 10:38:19 +0200

    navbar initialisation

--- Code/view/index.php
+++ Code/view/index.php
@@ -63,10 +63,71 @@
                 <a class="sf-menu-toggle fa fa-bars" href="#"></a>
                 <ul class="sf-menu">
                     <li class="active">
-                        <a href="./">Home</a>
+                        <a href="./">Lowx</a>
                     </li>
                     <li>
-                        <a href="index-1.php">About</a>
+                        <a href="index-1.php">Qui sommes-nous?</a>
+                        <ul>
+                          <li>
+                              <a href="#">Pierre</a>
+                          </li>
+                          <li>
+                              <a href="#">Henri</a>
+                          </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="index-1.php">Patisserie</a>
+                        <ul>
+                          <li>
+                              <a href="#">Catégorie 1</a>
+                          </li>
+                          <li>
+                              <a href="#">Catégorie 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="index-1.php">Restaurant</a>
+                        <ul>
+                          <li>
+                              <a href="#">Entree</a>
+                          </li>
+                          <li>
+                              <a href="#">Plat</a>
+                          </li>
+                          <li>
+                              <a href="#">Boisson</a>
+                          </li>
+                          <li>
+                              <a href="#">Dessert</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="index-1.php">Epicerie</a>
+                        <ul>
+                          <li>
+                              <a href="#">Gamme 1</a>
+                          </li>
+                          <li>
+                              <a href="#">Gamme 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="index-1.php">Contacts</a>
+                        <ul>
+                          <li>
+                              <a href="#">Pierre</a>
+                          </li>
+                          <li>
+                              <a href="#">Henri</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="index-1.php">Connexion</a>
                         <ul>
                             <li>
                                 <a href="#">Quisque nulla</a>
