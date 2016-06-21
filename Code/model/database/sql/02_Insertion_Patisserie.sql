e4ef456e4b1806efd27222314d83318d9b26d968
HighX97 <loic.ortole@gmail.com>
2016-06-21 17:09:49 +0200

    Patisserie images 100%

--- Code/model/database/sql/02_Insertion_Patisserie.sql
+++ Code/model/database/sql/02_Insertion_Patisserie.sql
@@ -1,8 +1,8 @@
 /*
 Categorie
 
-INSERT INTO Catégorie (libelle, description, prix)
-VALUES (,1.80);
+INSERT INTO Categorie (libelle, description, prix, image)
+VALUES (,1.80,"images/patisserie/");
 SELECT * FROM Categorie;
 */
 
@@ -10,51 +10,51 @@ SELECT * FROM Categorie;
 Patisserie
 */
 INSERT INTO Patisserie(libelle, description, prix, image)
-VALUES ("Baclawa amandes, pistaches ou noix", "Pâtes feuilletées croustillantes farcies de fruits secs concassés et arrosées de miel.",1.80, "images/restaurant/Boisson/eau_cristalline_50CL.jpg");
+VALUES ("Baclawa amandes, pistaches ou noix", "Pates feuilletees croustillantes farcies de fruits secs concasses et arrosees de miel.",1.80, "images/patisserie/Baclawa amandes.JPG");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Chebakia sésame et miel:","Gâteau enrobé de sésame, arrosé de miel et légèrement parfumé à l'anis",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Chebakia sesame et miel","Gateau enrobe de sesame, arrose de miel et legerement parfume à l'anis",1.80,"images/patisserie/Chebakia sesame et miel.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Cigare noix de cajous ou amandes", "Pâte feuilletées farcies de noix de cajous et arrosées de miel.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Cigare noix de cajous ou amandes", "Pate feuilletees farcies de noix de cajous et arrosees de miel.",1.80,"images/patisserie/Cigare noix de cajous ou amandes.JPG");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Corne de gazelle libanaise:","Biscuit sablé farci d'amandes et aux éclats de noix, enrobé de sucre glace.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Corne de gazelle libanaise","Biscuit sable farci d'amandes et aux eclats de noix, enrobe de sucre glace.",1.80,"images/patisserie/Corne de gazelle libanaise.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Corne de gazelle marocaine","Biscuit fondant farci d'amandes et parfumé à la fleur d'oranger.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Corne de gazelle marocaine","Biscuit fondant farci d'amandes et parfume à la fleur d'oranger.",1.80,"images/patisserie/Corne de gazelle marocaine.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Corne de gazelle tunisienne","Biscuit sablé farci d'amandes concassés et enrobés de sucre glace.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Corne de gazelle tunisienne","Biscuit sable farci d'amandes concasses et enrobes de sucre glace.",1.80,"images/patisserie/Corne de gazelle tunisienne.JPG");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Galbellouz semoule et miel","Semoule et miel au coeur fondant à l'amande et parfumé à la fleur d'oranger.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Galbellouz semoule et miel","Semoule et miel au coeur fondant à l'amande et parfume à la fleur d'oranger.",1.80,"images/patisserie/Galbellouz semoule et miel.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Kadaïf noix ou amandes:","Cheveux d'ange farcis de fruits secs concassés et de miel.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Kadaif noix ou amandes","Cheveux d'ange farcis de fruits secs concasses et de miel.",1.80,"images/patisserie/Kadaif noix ou amandes.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Losanges amandes, pistaches ou noix:","Pâtes feuilletées fondantes farcies de fruits secs finement concassés et arrosés de miel.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Losanges amandes, pistaches ou noix","Pates feuilletees fondantes farcies de fruits secs finement concasses et arroses de miel.",1.80,"images/patisserie/Losanges amandes, pistaches ou noix.JPG");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Makroudhs dattes:","Semoule de blé dur, farci de pâte de datte et enrobé de miel.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Makroudhs dattes","Semoule de ble dur, farci de pate de datte et enrobe de miel.",1.80,"images/patisserie/Makroudhs dattes.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Mamoul datte, pistaches ou noix","Biscuit sablé libanais farci à la datte, pistache ou noix.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Mamoul datte, pistaches ou noix","Biscuit sable libanais farci à la datte, pistache ou noix.",1.80,"images/patisserie/Mamoul datte, pistaches ou noix.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Montecao cannelle:","Biscuit sablé parfumé à la cannelle.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Montecao cannelle","Biscuit sable parfume à la cannelle.",1.80,"images/patisserie/Montecao cannelle.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Nid de pistache:", "Pâte d'amande et de noix enrobée de pistaches concassées et de sucre glace.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Nid de pistache", "Pate d'amande et de noix enrobee de pistaches concassees et de sucre glace.",1.80,"images/patisserie/Nid de pistache.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Nid d'oiseau:","Cheveux d'ange garnis de pistaches entières caramélisées.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Nid d'oiseau","Cheveux d'ange garnis de pistaches entieres caramelisees.",1.80,"images/patisserie/Nid d'oiseau.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Wardi pistache:","Pâtes feuilletés avec des pistaches entières arrosées de miel.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Wardi pistache","Pates feuilletes avec des pistaches entieres arrosees de miel.",1.80,"images/patisserie/Wardi pistache.jpg");
 
-INSERT INTO Patisserie(libelle, description, prix)
-VALUES ("Zlabia","Beignet de miel souvent coloré en rouge.",1.80);
+INSERT INTO Patisserie(libelle, description, prix, image)
+VALUES ("Zlabia","Beignet de miel souvent colore en rouge.",1.80,"images/patisserie/Zlabia.jpg");
 
 SELECT * FROM Patisserie;
