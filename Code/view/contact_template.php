5e4fbde3449c88908d61fc2c523a7a6f29456846
P3X747 <maerre@hotmail.fr>
2016-06-24 17:42:57 +0200

    contact template.php map 50%

--- Code/view/contact_template.php
+++ Code/view/contact_template.php
@@ -59,7 +59,7 @@
                 <div class="map">
                   <div id="google-map" class="map_model"></div>
                   <ul class="map_locations">
-                    <li data-x="-73.9874068" data-y="40.643180">
+                    <li data-x="3.877105" data-y="43.607215">
                       <p> 9870 St Vincent Place, Glasgow, DC 45 Fr 45. <span>800 2345-6789</span></p>
                     </li>
                   </ul>
