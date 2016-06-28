8ad0342c4bb54f97341ba41e0970cf2f1ae6ee3c
P3X747 <maerre@hotmail.fr>
2016-06-28 15:45:54 +0200

    restaurant_view.php suppression fonction boisson

--- Code/view/restaurant_view.php
+++ Code/view/restaurant_view.php
@@ -175,47 +175,5 @@ class Restaurant_view extends Abstract_view
       echo '</div>';
     }
   }
-
-  public function display_list_Boisson()
-  {
-    //Appel de la fonction getFormule ()
-    //Enregistrement du résultat dans la variable $rslt
-    $rslt = $this->db_model->getBoisson();
-    //Si il y a au moins une ligne
-    if ($rslt->num_rows > 0) {
-      //Variable $i enregistre le numero de la ligne à afficher
-      $i = 0;
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt->fetch_assoc())
-      {
-        $this->display_ligne_Boisson($i,$row["libelle"],$row["description"],$row["prix"]);
-        $i =  $i + 1;
-      }
-    }
-    else
-    {
-      echo "0 results";
-    }
-  }
-
-  public function display_ligne_Boisson($i,$libelle,$description,$prix)
-  {
-    if ($i%3==0)
-    {
-      echo '<div class="row box-2">';
-    }
-    echo '<div class="grid_4">
-    <div class="img"><div class="lazy-img" style="padding-bottom: 76.21621621621622%;"><img data-src="images/page-4_img0'.($i%9+1).'.jpg" alt=""></div></div>
-    <h3>'.$libelle.'</h3>
-    <p>'.$description.'</p>
-    <h3>'.$prix.' euros </h3>
-    <a href="#" class="btn">Read more</a>
-    </div>';
-    if ($i%3==2)
-    {
-      echo '</div>';
-    }
-  }
-
 }
 ?>
