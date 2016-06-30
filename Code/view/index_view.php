658cea6beaeaf1d54b9c35e6bbc29371df176bfe
HighX97 <loic.ortole@gmail.com>
2016-06-30 13:34:54 +0200

    Try to fix issues

--- Code/view/index_view.php
+++ Code/view/index_view.php
@@ -12,7 +12,7 @@ class Index_view extends Abstract_view
     <title>Home</title>
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=no"/>
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href=http:\\localhost\Lautre_sud\Code\view\template\images\favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/camera.css">
@@ -111,203 +111,168 @@ class Index_view extends Abstract_view
     return $index_header;
   }
 
-  public function display_list_ElementCarte()
+  public function get_index_main()
   {
-    //Appel de la fonction getAllElementCarte ()
-    //Enregistrement du résultat dans la variable $rslt
-    $rslt = $this->db_model->getAllElementCarte ();
-    //Si il y a au moins une ligne
-    if ($rslt->num_rows > 0) {
-      //Variable $i enregistre le numero de la ligne à afficher
-      $i = 0;
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt->fetch_assoc())
-      {
-        $this->display_ligne_ElementCarte($i,$row["libelle"],$row["description"],$row["prix"]);
-        $i =  $i + 1;
-      }
-    }
-    else
-    {
-      echo "0 results";
-    }
-  }
-  public function display_menu_Restaurant()
-  {
-    echo'<li>
-      <a href="restaurant_template.php">Restaurant</a>
-      <ul>';
-      //Appel de la fonction getAllElementCarte ()
-      $rslt_type = $this->db_model->getType();
-      //Si il y a au moins une ligne
-      if ($rslt_type->num_rows > 0) {
-        //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-        while($row = $rslt_type->fetch_assoc())
-        {
-          echo'
-          <li>
-          <a href="restaurant_template.php#'.$row["libelle"].'">'.$row["libelle"].'</a>
-          </li>';
-          }
-        }
-      echo'
-      </ul>
-      </li>' ;
-  }
-
-
-  public function get_list_ElementCarte_by_type()
-  {
-    $list_ElementCarte_by_type ="";
-    //Appel de la fonction getAllElementCarte ()
-    $rslt_type = $this->db_model->getType();
-    //Si il y a au moins une ligne
-    if ($rslt_type->num_rows > 0) {
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt_type->fetch_assoc())
-      {
-        $rslt = $this->db_model->getElementCarte($row["libelle"]);
-        $list_ElementCarte_by_type = $list_ElementCarte_by_type . '<h2 id="'.$row["libelle"].'"><em>'.$row["libelle"].'</em></h2>';
-        //Si il y a au moins une ligne
-        if ($rslt->num_rows > 0)
-        {
-          //Variable $i enregistre le numero de la ligne à afficher
-          $i = 0;
-          //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-          while($row = $rslt->fetch_assoc())
-          {
-            $list_ElementCarte_by_type = $list_ElementCarte_by_type.$this->get_ligne_ElementCarte($i,$row["libelle"],$row["description"],$row["prix"],$row["image"]);
-            $i =  $i + 1;
-          }
-          if ($i%3!=0)
-          {
-            $list_ElementCarte_by_type = $list_ElementCarte_by_type.'</div>';
-          }
-        }
-        else
-        {
-          $list_ElementCarte_by_type = $list_ElementCarte_by_type."0 results";
-        }
-      }
-    }
-    return $list_ElementCarte_by_type;
-  }
-  public function display_list_ElementCarte_by_type()
-  {
-    //Appel de la fonction getAllElementCarte ()
-    $rslt_type = $this->db_model->getType();
-    //Si il y a au moins une ligne
-    if ($rslt_type->num_rows > 0) {
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt_type->fetch_assoc())
-      {
-        $rslt = $this->db_model->getElementCarte($row["libelle"]);
-        echo '<h2 id="'.$row["libelle"].'"><em>'.$row["libelle"].'</em></h2>';
-        //Si il y a au moins une ligne
-        if ($rslt->num_rows > 0)
-        {
-          //Variable $i enregistre le numero de la ligne à afficher
-          $i = 0;
-          //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-          while($row = $rslt->fetch_assoc())
-          {
-            $this->display_ligne_ElementCarte($i,$row["libelle"],$row["description"],$row["prix"],$row["image"]);
-            $i =  $i + 1;
-          }
-          if ($i%3!=0)
-          {
-            echo '</div>';
-          }
-        }
-        else
-        {
-          echo "0 results";
-        }
-      }
-    }
-  }
-  public function display_list_Type()
-  {
-    //Appel de la fonction getType ()
-    //Enregistrement du résultat dans la variable $rslt
-    $rslt = $this->db_model->getType();
-    //Si il y a au moins une ligne
-    if ($rslt->num_rows > 0) {
-      //Variable $i enregistre le numero de la ligne à afficher
-      $i = 0;
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt->fetch_assoc())
-      {
-        $this->display_ligne_Type($i,$row["libelle"],$row["description"]);
-        $i =  $i + 1;
-      }
-      if ($i%3!=2)
-      {
-        echo '</div>';
-      }
-    }
-    else
-    {
-      echo "0 results";
-    }
-  }
-  public function display_ligne_Type($i,$libelle,$description)
-  {
-    if ($i%3==0)
-    {
-      echo '<div class="row box-2">';
-    }
-    echo '<div class="grid_4">
-    <div class="img"><div class="lazy-img" style="padding-bottom: 76.21621621621622%;"><img data-src="images/page-4_img0'.($i%9+1).'.jpg" alt=""></div></div>
-    <h3>'.$libelle.'</h3>
-    <p>'.$description.'</p>
-    <a href="#" class="btn">Read more</a>
-    </div>';
-    if ($i%3==2)
-    {
-      echo '</div>';
-    }
-  }
-  public function display_list_Formule()
-  {
-    //Appel de la fonction getFormule ()
-    //Enregistrement du résultat dans la variable $rslt
-    $rslt = $this->db_model->getFormule();
-    //Si il y a au moins une ligne
-    if ($rslt->num_rows > 0) {
-      //Variable $i enregistre le numero de la ligne à afficher
-      $i = 0;
-      //La fonction fetch_assoc() permet de lire ligne par ligne le résultat de la requete sql
-      while($row = $rslt->fetch_assoc())
-      {
-        $this->display_ligne_Formule($i,$row["libelle"],$row["description"],$row["prix"]);
-        $i =  $i + 1;
-      }
-    }
-    else
-    {
-      echo "0 results";
-    }
+    $index_main = '<main>
+    <section class="well">
+    <div class="container">
+    <h2><em>Welcome</em>to Our Place</h2>
+    <div class="row">
+    <div class="grid_6">
+    <div class="img img__border"><div class="lazy-img" style="padding-bottom: 63.0282%;"><img data-src="images/restaurant/Boisson/cafe_espresso_malongo.jpg" alt=""></div></div>
+    <p class="center indents-1">Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla.</p>
+    </div>
+    <div class="grid_6">
+    <div class="img img__border"><div class="lazy-img" style="padding-bottom: 63.0282%;"><img data-src="images/page-1_img02.jpg" alt=""></div></div>
+    <p class="center indents-1">Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla.</p>
+    </div>
+    </div>
+    <div class="decoration"><a href="#" class="btn">Read more</a></div>
+    <h2><em>Our</em>Cuisine</h2>
+    </div>
+    <div class="gallery">
+    <div class="gallery_col-1">
+    <a data-fancybox-group="gallery" href="images/page-1_img03_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 93.96551724137931%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img03.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img04_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 74.13793103448276%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img04.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img05_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 94.6551724137931%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img05.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    </div>
+    <div class="gallery_col-2">
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img06_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 52.48322147651007%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img06.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img07_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 55.97315436241611%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img07.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img08_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 96.10738255033557%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img08.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    </div>
+    <div class="gallery_col-3">
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img09_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 93.69676320272572%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img09.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img10_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 72.23168654173765%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img10.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    <a data-fancybox-group="gallery" href=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img11_original.jpg" class="gallery_item thumb lazy-img" style="padding-bottom: 93.69676320272572%;">
+    <img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img11.jpg" alt="">
+    <div class="gallery_overlay">
+    <div class="gallery_caption">
+    <p><em>Lorem Blandit</em></p>
+    <p>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl</p>
+    </div>
+    </div>
+    </a>
+    </div>
+    </div>
+    </section>
+    <section class="parallax parallax1" data-parallax-speed="-0.4">
+    <div class="container">
+    <h2><em>Our </em>Experience</h2>
+    <p class="indents-2">Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci </p>
+    <a href="#" class="btn">View full menu</a>
+    </div>
+    </section>
+    <section class="well well__offset-1 bg-1">
+    <div class="container">
+    <h2><em>Our </em>Cooks</h2>
+    <div class="row row__offset-1">
+    <div class="grid_4">
+    <figure>
+    <div class="img lazy-img" style="padding-bottom: 101.0810810810811%;"><img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img12.jpg" alt=""></div>
+    <figcaption>Kevin Grey</figcaption>
+    </figure>
+    <h3>Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl </h3>
+    <p>Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla.</p>
+    </div>
+    <div class="grid_4">
+    <figure>
+    <div class="img lazy-img" style="padding-bottom: 101.0810810810811%;"><img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img13.jpg" alt=""></div>
+    <figcaption>Linda Klein</figcaption>
+    </figure>
+    <h3>Oeteger convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl </h3>
+    <p>Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla.</p>
+    </div>
+    <div class="grid_4">
+    <figure>
+    <div class="img lazy-img" style="padding-bottom: 101.0810810810811%;"><img data-src=http:\\localhost\Lautre_sud\Code\view\template\images\page-1_img14.jpg" alt=""></div>
+    <figcaption>Ann Shelton</figcaption>
+    </figure>
+    <h3>Koteger convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl </h3>
+    <p>Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla.</p>
+    </div>
+    </div>
+    <div class="decoration"><a href="#" class="btn">Read more</a></div>
+    </div>
+    </section>
+    <section class="well well__offset-2">
+    <div class="container center">
+    <h2><em>Make </em>a Reservation</h2>
+    <p class="indents-2">Fnteger convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol. sed,pharetra venenatis nulla. Vestibulum volutpat turpis ut massa commodo, quis aliquam massa facilisis.Integer convallis orci vel mi nelaoreet, at ornare lorem consequat. Phasellus era nisl auctor vel veliterol.</p>
+    <address class="address-1">
+    <dl><dt>Address:</dt> <dd>4578 Marmora Road, Glasgow DA04 89GR</dd></dl>
+    <p><em>800 2345-6789</em></p>
+    </address>
+    </div>
+    </section>
+    </main>';
+    return $index_main;
   }
 
 
-  public function display_ligne_Formule($i,$libelle,$description,$prix)
-  {
-    if ($i%3==0)
-    {
-      echo '<div class="row box-2">';
-    }
-    echo '<div class="grid_4">
-    <div class="img"><div class="lazy-img" style="padding-bottom: 76.21621621621622%;"><img data-src="images/page-4_img0'.($i%9+1).'.jpg" alt=""></div></div>
-    <h3>'.$libelle.'</h3>
-    <p>'.$description.'</p>
-    <h3>'.$prix.' euros </h3>
-    <a href="#" class="btn">Read more</a>
-    </div>';
-    if ($i%3==2)
-    {
-      echo '</div>';
-    }
-  }
 }
 ?>
